53f9d219c98b3360134e0c5050a7044412311ab8
Angie Kassab <anasab12@gmail.com>
2018-04-25 03:24:45 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,7 +7,9 @@ def display_rainbow(colors) #method(argument)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
   =begin
    #{colors[0]} is red
-   #{colors[1]} is red
+   #{colors[1]} is orange
+   ... and so on
+   
   
   
 end
